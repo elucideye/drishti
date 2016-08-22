31466ba79d4b8f43ea78b84cd5c3bcab2ef589aa
Ruslan Baratov <ruslan_baratov@yahoo.com>
2016-08-22 22:15:26 +0300

    Remove unused

--- cmake/Modules/ForEachPrepend.cmake
+++
@@ -1,7 +0,0 @@
-FUNCTION(for_each_prepend var prefix)
-  SET(listVar "")
-  FOREACH(f ${ARGN})
-      LIST(APPEND listVar "${prefix}/${f}")
-   ENDFOREACH(f)
-   SET(${var} "${listVar}" PARENT_SCOPE)
-ENDFUNCTION(for_each_prepend)
