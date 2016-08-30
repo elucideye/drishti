a5928385f348ef222bb8292e10e53484f49fc8cd
Ruslan Baratov <ruslan_baratov@yahoo.com>
2016-08-30 17:52:37 +0300

    Remove trailing whitespaces

--- cmake/Modules/DrishtiConfigureConsoleApp.cmake
+++ cmake/Modules/DrishtiConfigureConsoleApp.cmake
@@ -1,4 +1,4 @@
-function(configure_console_app 
+function(configure_console_app
     DRISHTI_SDK_PROJECT_NAME
     DRISHTI_SDK_PRODUCT_NAME
     DRISHTI_SDK_APP_NAME
@@ -8,11 +8,11 @@ function(configure_console_app
   message("DRISHTI_SDK_LIBS: ${DRISHTI_SDK_LIBS}")
 
   string(COMPARE EQUAL "${CMAKE_OSX_SYSROOT}" "iphoneos" is_ios)
-  
+
   if(is_ios)
-    # Simple iOS console application 
+    # Simple iOS console application
     configure_file("${CMAKE_SOURCE_DIR}/src/app/ios/boilerplate/empty_application/CMakeLists.txt.in" ${DRISHTI_SDK_PROJECT_NAME}.cmake @ONLY)
-    include("${CMAKE_CURRENT_BINARY_DIR}/${DRISHTI_SDK_PROJECT_NAME}.cmake")    
+    include("${CMAKE_CURRENT_BINARY_DIR}/${DRISHTI_SDK_PROJECT_NAME}.cmake")
   else() ### ANDROID or *
     # Simple *nix console application
     add_executable(${DRISHTI_SDK_APP_NAME} ${DRISHTI_SDK_SOURCES})
@@ -20,5 +20,5 @@ function(configure_console_app
     install(TARGETS ${DRISHTI_SDK_APP_NAME} DESTINATION bin)
     set_target_properties(${DRISHTI_SDK_APP_NAME} PROPERTIES INSTALL_RPATH "@loader_path/../lib")
   endif()
-  
+
 endfunction(configure_console_app)
