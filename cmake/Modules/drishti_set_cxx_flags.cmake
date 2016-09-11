c8aa1e29fd5824a3fb15e834b72d80e6a069a88a
David Hirvonen <dhirvonen@elucideye.com>
2016-09-11 13:52:17 -0400

    Don't use `-flag` syntax for MSVC

--- cmake/Modules/drishti_set_cxx_flags.cmake
+++ cmake/Modules/drishti_set_cxx_flags.cmake
@@ -1,25 +1,29 @@
 macro(drishti_set_cxx_flags)
 
-# -Wno-narrowing  = Don't warn about type narrowing
-include(CheckCXXCompilerFlag)
-CHECK_CXX_COMPILER_FLAG(-Wno-c++11-narrowing COMPILER_SUPPORTS_NO_NARROWING)
-if(COMPILER_SUPPORTS_NO_NARROWING)
-  set(CMAKE_CXX_FLAGS    "${CMAKE_CXX_FLAGS} -Wno-c++11-narrowing")
-endif()
+  if(NOT MSVC)
 
-CHECK_CXX_COMPILER_FLAG(-Wno-unused-parameter COMPILER_SUPPORTS_NO_UNUSED_PARAMETER)
-if(COMPILER_SUPPORTS_NO_UNUSED_PARAMETER)
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unused-parameter")
-endif()
+    # -Wno-narrowing  = Don't warn about type narrowing
+    include(CheckCXXCompilerFlag)
+    CHECK_CXX_COMPILER_FLAG(-Wno-c++11-narrowing COMPILER_SUPPORTS_NO_NARROWING)
+    if(COMPILER_SUPPORTS_NO_NARROWING)
+      set(CMAKE_CXX_FLAGS    "${CMAKE_CXX_FLAGS} -Wno-c++11-narrowing")
+    endif()
 
-CHECK_CXX_COMPILER_FLAG(-Wno-unused-local-typedefs COMPILER_SUPPORTS_NO_UNUSED_LOCAL_TYPEDEFS)
-if(COMPILER_SUPPORTS_NO_UNUSED_LOCAL_TYPEDEFS)
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unused-local-typedefs")
-endif()
+    CHECK_CXX_COMPILER_FLAG(-Wno-unused-parameter COMPILER_SUPPORTS_NO_UNUSED_PARAMETER)
+    if(COMPILER_SUPPORTS_NO_UNUSED_PARAMETER)
+      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unused-parameter")
+    endif()
 
-CHECK_CXX_COMPILER_FLAG(-Wno-unknown-attributes COMPILER_SUPPORTS_NO_UNKNOWN_ATTRIBUTES)
-if(COMPILER_SUPPORTS_NO_UNKNOWN_ATTRIBUTES)
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unknown-attributes")
-endif()
+    CHECK_CXX_COMPILER_FLAG(-Wno-unused-local-typedefs COMPILER_SUPPORTS_NO_UNUSED_LOCAL_TYPEDEFS)
+    if(COMPILER_SUPPORTS_NO_UNUSED_LOCAL_TYPEDEFS)
+      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unused-local-typedefs")
+    endif()
+
+    CHECK_CXX_COMPILER_FLAG(-Wno-unknown-attributes COMPILER_SUPPORTS_NO_UNKNOWN_ATTRIBUTES)
+    if(COMPILER_SUPPORTS_NO_UNKNOWN_ATTRIBUTES)
+      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unknown-attributes")
+    endif()
+
+  endif()
 
 endmacro()
