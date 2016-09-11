ef9628ed0a5bda99b8b2fe5829b357ab82ba3485
David Hirvonen <dhirvonen@elucideye.com>
2016-09-11 10:21:33 -0400

    add drishti_math.h (initially for M_PI on non posix systems)

---
+++ src/lib/drishti/core/drishti_math.h
@@ -0,0 +1,19 @@
+/*!
+  @file   drishti_math.h
+  @author David Hirvonen
+  @brief  Portable math constants.
+
+  \copyright Copyright 2014-2016 Elucideye, Inc. All rights reserved.
+  \license{This project is released under the 3 Clause BSD License.}
+
+*/
+
+#ifndef drishti_core_drishti_math_h
+#define drishti_core_drishti_math_h 1
+
+#ifndef M_PI
+// Source: http://www.geom.uiuc.edu/~huberty/math5337/groupe/digits.html
+#  define M_PI 3.141592653589793238462643383279502884197169399375105820974944592307816406 
+#endif
+
+#endif // drishtisdk_core_drishti_math_h
