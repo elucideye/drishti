48ea598238be88109ab98afccc811f4dac68aa10
David Hirvonen <dhirvonen@elucideye.com>
2016-09-15 23:05:12 -0400

    include math.h in drishti_math.h

--- src/lib/drishti/core/drishti_math.h
+++ src/lib/drishti/core/drishti_math.h
@@ -11,17 +11,6 @@
 #ifndef drishti_core_drishti_math_h
 #define drishti_core_drishti_math_h 1
 
-#ifndef M_PI
-// Source: http://www.geom.uiuc.edu/~huberty/math5337/groupe/digits.html
-#  define M_PI 3.141592653589793238462643383279502884197169399375105820974944592307816406 
-#endif
-
-#ifndef M_SQRT2
-#  define M_SQRT2 1.41421356237309504880
-#endif
-
-#ifndef M_PI_2
-#  define M_PI_2 1.57079632679489661923
-#endif
+#include <math.h> // for math constants
 
 #endif // drishtisdk_core_drishti_math_h
