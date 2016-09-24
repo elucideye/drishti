4025fbc6867ac8504e92fa62a79878748f2c5dca
David Hirvonen <dhirvonen@elucideye.com>
2016-09-24 19:20:02 -0400

    Update drishti_symbol_list.cmake

--- cmake/Modules/drishti_symbol_list.cmake
+++ cmake/Modules/drishti_symbol_list.cmake
@@ -11,13 +11,13 @@ function(drishti_symbol_list drishti_library)
 
   if (APPLE)
     # Create a symbols_list file for the darwin linker
-    string(REPLACE ";" "\n_" _symbols "${SDK_SYMBOLS}")
+    string(REPLACE ";" "\n_" _symbols "${DRISHTI_SDK_SYMBOLS}")
     set(_symbols_list "${CMAKE_CURRENT_BINARY_DIR}/symbols.list")
     file(WRITE ${_symbols_list} "_${_symbols}\n")
     set(LINK_FLAGS "${LINK_FLAGS} -Wl,-exported_symbols_list,'${_symbols_list}'")
   elseif (CMAKE_C_COMPILER_ID STREQUAL GNU)
     # Create a version script for GNU ld.
-    set(_symbols "{ global: ${SDK_SYMBOLS}; local: *; };")
+    set(_symbols "{ global: ${DRISHTI_SDK_SYMBOLS}; local: *; };")
     set(_version_script "${CMAKE_CURRENT_BINARY_DIR}/version.script")
     file(WRITE ${_version_script} "${_symbols}\n")
     set(LINK_FLAGS "${LINK_FLAGS} -Wl,--version-script,'${_version_script}'")
