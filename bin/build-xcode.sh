2ac4c4e15d3ae268a36b27182e201440ce5949eb
David Hirvonen <dhirvonen@elucideye.com>
2016-09-24 12:34:38 -0400

    Use xcode-sections toolchain

--- bin/build-xcode.sh
+++ bin/build-xcode.sh
@@ -2,7 +2,7 @@
 
 . ${DRISHTISDK}/bin/build-common.sh
 
-TOOLCHAIN=xcode
+TOOLCHAIN=xcode-sections
 
 EXTRA_ARGS=""
 if [ $# -ge 1 ]; then
@@ -10,15 +10,19 @@ if [ $# -ge 1 ]; then
     EXTRA_ARGS="--clear"
 fi
 
+DRISHTI_CONFIGURATION=Release
+#DRISHTI_CONFIGURATION=MinSizeRel
 DRISHTI_BUILD_QT=OFF
 DRISHTI_BUILD_OGLES_GPGPU=OFF
 DRISHTI_USE_TEXT_ARCHIVES=ON
 
 rename_tab drishti ${TOOLCHAIN}
 
+
+
 COMMAND=(
     "--verbose --fwd "
-    "HUNTER_CONFIGURATION_TYPES=Release "
+    "HUNTER_CONFIGURATION_TYPES=${DRISHTI_CONFIGURATION} "
     "${DRISHTI_BUILD_ARGS[*]} "
     "${DRISHTI_BUILD_HIDE[*]} "
     "CMAKE_XCODE_ATTRIBUTE_OSX_DEPLOYMENT_TARGET=10.10 "
@@ -32,7 +36,7 @@ COMMAND=(
     "--jobs 8 "
     "--open "
     "--install "
-    "${EXTRA_ARGS}  --nobuild"
+    "${EXTRA_ARGS} " # --nobuild"
 )
 
 build.py --toolchain ${TOOLCHAIN} ${COMMAND[*]}
