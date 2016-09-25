755d32094fe0d15ce0e1349b84f227110887b1a1
David Hirvonen <dhirvonen@elucideye.com>
2016-09-24 23:06:54 -0400

    add function to mirror a boolean option/variable with a compiler definition

---
+++ cmake/Modules/drishti_mirror_definition.cmake
@@ -0,0 +1,7 @@
+function(drishti_mirror_definition varName)
+  if(${varName})
+    add_definitions(-D${${varName}}=1)
+  else()
+    add_definitions(-D${${varName}}=0)
+  endif()
+endfunction(drishti_mirror_definition)
