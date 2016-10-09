ecbfb31ddb1b90d77b523b2ae7d3b0b3a1bf4afc
David Hirvonen <dhirvonen@elucideye.com>
2016-10-09 12:10:18 -0400

    add TGZ pack step for travis deployment

---
+++ appveyor.cmd
@@ -0,0 +1,7 @@
+python %POLLY_ROOT%\bin\polly.py ^
+--verbose ^
+--pack TGZ ^
+--config "%CONFIG%" ^
+--toolchain "%TOOLCHAIN%" ^
+--test ^
+--fwd HUNTER_USE_CACHE_SERVERS=YES DRISHTI_USE_TEXT_ARCHIVES=YES
