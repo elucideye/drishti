cb217b1713c2c41bc6d541e984bbe4e71522268a
David Hirvonen <dhirvonen@elucideye.com>
2016-10-09 14:05:53 -0400

    add build-appveyor.cmd bat for readable multi-line polly.py command
    Note: Previous attempt used appveyor.cmd which seems to conflict with appveyor.yml

---
+++ bin/build-appveyor.cmd
@@ -0,0 +1,15 @@
+:: Name: build-appveyor.cmd
+:: Purpose: Support readable multi-line polly.py build commands
+:: Copyright 2016 Elucideye, Inc.
+::
+:: Multi-line commands are not currently supported directly in appveyor.yml files
+::
+:: See: http://stackoverflow.com/a/37647169
+
+python %POLLY_ROOT%\bin\polly.py ^
+--verbose ^
+--pack TGZ ^
+--config "%1%" ^
+--toolchain "%2%" ^
+--test ^
+--fwd HUNTER_USE_CACHE_SERVERS=YES DRISHTI_USE_TEXT_ARCHIVES=YES
