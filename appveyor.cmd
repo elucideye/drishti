fc87b41e10651a3f46ec28b8871e496c5d6c0bfd
David Hirvonen <dhirvonen@elucideye.com>
2016-10-09 12:59:20 -0400

    add header to appveyor.cmd; try call syntax

--- appveyor.cmd
+++ appveyor.cmd
@@ -1,3 +1,11 @@
+:: Name: appveyor.cmd
+:: Purpose: Support readable multi-line polly.py build commands
+:: Copyright 2016 Elucideye, Inc.
+::
+:: Multi-line commands are not currently supported directly in appveyor.yml files
+::
+:: See: http://stackoverflow.com/a/37647169
+
 set POLLY_CMD=%1
 set CONFIG=%2
 set TOOLCHAIN=%3
