9145f79c7c1ff4a57998de0e8dfedea8c192219f
David Hirvonen <dhirvonen@elucideye.com>
2016-10-29 15:16:39 -0400

    remove top level appveyor.cmd file
    Apparently this gets run directly if present (instead of appveyor.yml)

--- appveyor.cmd
+++
@@ -1,8 +0,0 @@
-python %POLLY_ROOT%\bin\polly.py ^
---verbose ^
---pack TGZ ^
---config "%CONFIG%" ^
---toolchain "%TOOLCHAIN%" ^
---test ^
---fwd HUNTER_USE_CACHE_SERVERS=YES DRISHTI_USE_TEXT_ARCHIVES=YES --install
-
