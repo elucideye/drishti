9382e49c995bfd31b839b834a680bad2106ad4bb
David Hirvonen <dhirvonen@elucideye.com>
2016-11-11 10:44:42 -0500

    add simple msvc.bat file build script for reference

---
+++ bin/msvc.bat
@@ -0,0 +1,8 @@
+set PATH=c:\\Users\dhirv\AppData\\Local\\Programs\\Python\\Python35;c:\\Program Files\\CMake\\bin;%PATH%
+
+set POLLY_ROOT=%cd%\..\polly
+set HUNTER_ROOT=%cd%\..\hunter
+
+set CONFIG=Debug
+set TOOLCHAIN=vs-14-2015-win64
+python %POLLY_ROOT%\bin\polly.py --reconfig --verbose --pack TGZ --config "%CONFIG%" --toolchain "%TOOLCHAIN%" --test --fwd HUNTER_USE_CACHE_SERVERS=YES DRISHTI_USE_TEXT_ARCHIVES=YES  --nobuild --open
