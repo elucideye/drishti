c066b4c942b0aae7d33cd416ba0ef85237a9bcee
David Hirvonen <dhirvonen@elucideye.com>
2016-11-18 22:41:49 -0500

    add ml unit test

---
+++ src/lib/drishti/ml/ut/test-drishti-ml.cpp
@@ -0,0 +1,23 @@
+/*!
+  @file   test-ml.cpp
+  @author David Hirvonen
+  @brief  Google test for public drishti API.
+
+  \copyright Copyright 2016 Elucideye, Inc. All rights reserved.
+  \license{This project is released under the 3 Clause BSD License.}
+
+*/
+
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(XGBooster, XGBoosterInit)
+{
+    XGBooster booster;
+    ASSERT_EQ(true, true);
+}
