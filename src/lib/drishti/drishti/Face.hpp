0f4f96c9845c19e219c5d0a1f2c04b2a12097ab5
David Hirvonen <dhirvonen@elucideye.com>
2016-11-28 08:38:50 -0500

    add place holder public API Face class

---
+++ src/lib/drishti/drishti/Face.hpp
@@ -0,0 +1,45 @@
+/*!
+  @file   Face.hpp
+  @author David Hirvonen
+  @brief  Top level API eye model declaration.
+
+  \copyright Copyright 2014-2016 Elucideye, Inc. All rights reserved.
+  \license{This project is released under the 3 Clause BSD License.}
+
+  This file contains the declaration of the eye model structure used
+  to report results for the top level SDK.
+*/
+
+#ifndef DRISHTI_DRISHTI_FACE_H
+#define DRISHTI_DRISHTI_FACE_H 1
+
+#include "drishti/drishti_sdk.hpp"
+#include "drishti/Eye.hpp"
+#include "drishti/Image.hpp"
+
+#include <array>
+#include <iostream>
+
+_DRISHTI_SDK_BEGIN
+
+/*
+ * Eye type
+ */
+
+class DRISHTI_EXPORTS Face
+{
+public:
+
+    Face() {}
+
+    Eye & getEye(int i) { return m_eyes[i]; }
+    const Eye & getEye(int i) const { return m_eyes[i]; }
+
+protected:
+
+    std::array<Eye,2> m_eyes;
+};
+
+_DRISHTI_SDK_END
+
+#endif // DRISHTI_DRISHTI_FACE_H
