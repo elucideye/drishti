0e1d4eca0d4d7e692ebf3ac45f1f1d93e4096d05
David Hirvonen <dhirvonen@elucideye.com>
2016-12-04 12:45:36 -0500

    disable XCODE_ATTRIBUTE_GENERATE_MASTER_OBJECT_FILE setting for now
    This causes a linker crash in Xcode 8.1 in combination with thread_local storage and shared libraries

--- cmake/Modules/drishti_strip.cmake
+++ cmake/Modules/drishti_strip.cmake
@@ -8,6 +8,13 @@ function(drishti_strip drishti_library)
     XCODE_ATTRIBUTE_STRIPFLAGS "-x -u -r"
     XCODE_ATTRIBUTE_DEAD_CODE_STRIPPING "YES"
     XCODE_ATTRIBUTE_DEPLOYMENT_POSTPROCESSING "YES"
-    XCODE_ATTRIBUTE_GENERATE_MASTER_OBJECT_FILE "YES" # "Perform Single-Object Prelink"
+    # Note: Using this option in combination with thread_local and shared libraries crashes
+    # the linker step with the following error:
+    #
+    # ld: Assertion failed: (0 && "need to handle arm64 -r reloc"), function encodeSectionReloc,
+    # file /Library/Caches/com.apple.xbs/Sources/ld64/ld64-264.3.102/src/ld/LinkEditClassic.hpp, line 1907.
+    #
+    # This should can be enabled optionally for modules that do not require thread_local.
+    # XCODE_ATTRIBUTE_GENERATE_MASTER_OBJECT_FILE "YES" # "Perform Single-Object Prelink"
     )
 endfunction(drishti_strip)
