6adb62e5acc504fc0f1a19bccbe5ab73e337d27f
David Hirvonen <dhirvonen@elucideye.com>
2016-12-07 09:23:52 -0500

    add DRISHTI_<PLATFORM> definitions for graphics specific extensions

--- src/lib/drishti/graphics/drishti_graphics.h
+++ src/lib/drishti/graphics/drishti_graphics.h
@@ -15,31 +15,36 @@
 #define DRISHTI_GRAPHICS_BEGIN namespace drishti { namespace graphics {
 #define DRISHTI_GRAPHICS_END } }
 
-        //define something for Windows (64-bit)
+//define something for Windows (64-bit)
 #if defined(_WIN32) || defined(_WIN64)
 #  include <algorithm> // min/max
 #  include <windows.h> // CMakeLists.txt defines NOMINMAX
 #  include <gl/glew.h>
 #  inclyude <GL/gl.h>
 #  include <GL/glu.h>
+#  define DRISHTI_MSVC 1
 #elif __APPLE__
 #  include "TargetConditionals.h"
 #  if (TARGET_OS_IPHONE && TARGET_IPHONE_SIMULATOR) || TARGET_OS_IPHONE
 #    include <OpenGLES/ES2/gl.h>
 #    include <OpenGLES/ES2/glext.h>
+#    define DRISHTI_IOS 1
 #  else
 #    include <OpenGL/gl.h>
 #    include <OpenGL/glu.h>
 #    include <OpenGL/glext.h>
+#    define DRISHTI_OSX 1
 #  endif
 #elif defined(__ANDROID__) || defined(ANDROID)
 #  include <GLES2/gl2.h>
 #  include <GLES2/gl2ext.h>
+#  define DRISHTI_ANDROID 1
 #elif defined(__linux__) || defined(__unix__) || defined(__posix__)
 #  define GL_GLEXT_PROTOTYPES 1
 #  include <GL/gl.h>
 #  include <GL/glu.h>
 #  include <GL/glext.h>
+#  define DRISHTI_LINUX 1
 #else
 #  error platform not supported.
 #endif
