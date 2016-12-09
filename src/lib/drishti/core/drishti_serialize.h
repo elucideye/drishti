7709dc06796bdf0e18a0e0802a49c1f1e46c95b4
David Hirvonen <dhirvonen@elucideye.com>
2016-12-09 18:32:15 -0500

    add inline method to determine if archives are exported (by extension)

--- src/lib/drishti/core/drishti_serialize.h
+++ src/lib/drishti/core/drishti_serialize.h
@@ -1,9 +1,7 @@
 /*!
   @file   drishti_serialize.h
   @author David Hirvonen
-  @brief  Declaration of boost/cereal (de)serialization for common classes. This file also contains
-  implementations of standard library routines that are required by CEREAL and that are missing
-  on some platforms (notably ANDROID).
+  @brief  Declaration of serialization utility functions.
 
   \copyright Copyright 2014-2016 Elucideye, Inc. All rights reserved.
   \license{This project is released under the 3 Clause BSD License.}
@@ -11,6 +9,24 @@
 */
 
 #ifndef drishtisdk_drishti_serialize_h
+
+inline bool isArchiveSupported(const std::string &filename)
+{
+#if DRISHTI_SERIALIZE_WITH_BOOST
+    if(filename.find(".pba.z") != std::string::npos) return true;
+#endif
+    
+#if DRISHTI_SERIALIZE_WITH_BOOST && DRISHTI_USE_TEXT_ARCHIVES
+    if(filename.find(".txt") != std::string::npos) return true;
+#endif
+    
+#if DRISHTI_SERIALIZE_WITH_CEREAL
+    if(filename.find(".cpb") != std::string::npos) return true;
+#endif
+    
+    return false;
+}
+
 #define drishtisdk_drishti_serialize_h
 
 #endif
