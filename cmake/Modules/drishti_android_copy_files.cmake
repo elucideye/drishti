c421acd5cc1f1cacf7fa594daa5e979a1008edbf
David Hirvonen <dhirvonen@elucideye.com>
2016-12-21 11:56:50 -0500

    add local drishti_android_add_test
    Clone of: https://github.com/hunter-packages/android-apk/blob/master/AndroidApk.cmake
    
    Modified to set ${NAME}_INSTALL_PATH variable at PARENT_SCOPE so calling CMakeLists.txt can copy data to the correct path.
    
    For local configuration and ut experimenting
    
    Add local drishti_android_copy_files to to simply `adb push` of test data.

---
+++ cmake/Modules/drishti_android_copy_files.cmake
@@ -0,0 +1,43 @@
+function(drishti_android_copy_files TARGET LOCAL_FILES REMOTE_DIRECTORY)
+
+  if(HUNTER_ENABLED)
+    hunter_add_package(Android-SDK)
+    set(ADB_COMMAND "${ANDROID-SDK_ROOT}/android-sdk/platform-tools/adb")
+  else()
+    set(ADB_COMMAND "adb")
+  endif()
+  
+  set(VALID_LOCAL_FILES "")
+  foreach(local_file ${LOCAL_FILES})
+    message("?EXISTS: ${local_file}")
+    if(EXISTS "${local_file}")
+      message("EXISTS: ${local_file}")
+      list(APPEND VALID_LOCAL_FILES "${local_file}")
+
+      # NOTE: Currently running adb push one at a time...
+      # to be replaced by single adb push file1 file2 .. filen ${REMOTE_DIRECTORY}
+      # pending correct syntax (see below, escape issues, etc)
+      add_custom_command(
+        TARGET ${TARGET}
+        PRE_BUILD # Make sure this runs before the ctest step (POST_BUILD OK?)
+        COMMAND ${ADB_COMMAND} push "${local_file}" "${REMOTE_DIRECTORY}"
+        )
+      
+    endif()
+  endforeach()
+
+  # list(LENGTH VALID_LOCAL_FILES VALID_LOCAL_FILE_COUNT)
+  # message("VALID_LOCAL_FILE_COUNT: ${VALID_LOCAL_FILE_COUNT}")
+
+  # if(${VALID_LOCAL_FILE_COUNT})
+  #   string(REPLACE ";" " " VALID_LOCAL_FILES_STRING "${VALID_LOCAL_FILES}") # handle spaces?
+  #   message("adb push ${VALID_LOCAL_FILES_STRING} ${REMOTE_DIRECTORY}") 
+  #   add_custom_command(
+  #     TARGET ${TARGET}
+  #     POST_BUILD 
+  #     COMMAND ${ADB_COMMAND} push ${VALID_LOCAL_FILES_STRING} "${REMOTE_DIRECTORY}"
+  #     VERBATIM
+  #     )
+  # endif()
+  
+endfunction()
