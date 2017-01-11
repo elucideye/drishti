0d2071eaa9ddd94503cf9a08f8341c752281d309
David Hirvonen <dhirvonen@elucideye.com>
2017-01-11 14:24:14 -0500

    add testlib files: drishti_testlib.h, drishti_test_utils.h
    * drishti_testlib.h = standard namespace definitions
    * drishti_test_utils.h = add initial file for test support (file checks, etc)

---
+++ src/lib/drishti/testlib/drishti_test_utils.h
@@ -0,0 +1,39 @@
+/*!
+  @file   drishti_test_utils.h
+  @author David Hirvonen
+  @brief  Test utilities.
+
+  \copyright Copyright 2014-2016 Elucideye, Inc. All rights reserved.
+  \license{This project is released under the 3 Clause BSD License.}
+
+*/
+
+#ifndef __drishti_testlib_drishti_test_utils_h__
+#define __drishti_testlib_drishti_test_utils_h__
+
+#include "drishti/testlib/drishti_testlib.h"
+
+#include <vector>
+#include <fstream>
+#include <cassert>
+
+DRISHTI_TESTLIB_NAMESPACE_BEGIN
+
+inline bool hasFile(const std::string &filename)
+{
+    std::ifstream ifs(filename, std::ios::binary | std::ios::in);
+    return ifs.good();
+}
+
+inline bool hasFiles(char **argv, const std::vector<int> &index)
+{
+    for(const auto &i : index)
+    {
+        assert(hasFile(argv[i]));
+    }
+    return true;
+}
+
+DRISHTI_TESTLIB_NAMESPACE_END
+
+#endif
---
+++ src/lib/drishti/testlib/drishti_testlib.h
@@ -0,0 +1,17 @@
+/*!
+  @file   drishti_testlib.h
+  @author David Hirvonen
+  @brief  Internal ACF namespace.
+
+  \copyright Copyright 2014-2016 Elucideye, Inc. All rights reserved.
+  \license{This project is released under the 3 Clause BSD License.}
+
+*/
+
+#ifndef __drishti_testlib_drishti_testlib_h__
+#define __drishti_testlib_drishti_testlib_h__
+
+#define DRISHTI_TESTLIB_NAMESPACE_BEGIN namespace drishti { namespace testlib {
+#define DRISHTI_TESTLIB_NAMESPACE_END } }
+
+#endif
