e044454defcf16cd9a8f1664ba4450daca97d49b
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-01-30 19:54:25 +0800

    Remove trailing whitespaces

--- cmake/Modules/drishti_split_debug_symbols.cmake
+++ cmake/Modules/drishti_split_debug_symbols.cmake
@@ -15,25 +15,25 @@ function(drishti_split_debug_symbols lib_name)
       "${CMAKE_BINARY_DIR}/${lib_name}.dSYM"
       DESTINATION "${CMAKE_INSTALL_PREFIX}/.dSYM/"
       )
-    
+
   else()
 
     # see: https://sourceware.org/gdb/onlinedocs/gdb/Separate-Debug-Files.html
-    
+
     set(BUILDID "abcdef1234")
     string(SUBSTRING "${BUILDID}" 0 2 BUILDIDPREFIX)
     string(SUBSTRING "${BUILDID}" 2 8 BUILDIDSUFFIX)
 
     include(drishti_check_cxx_linker_flag)
     drishti_check_cxx_linker_flag("-Wl,--build-id=0x${BUILDID}" linker_supports_build_id)
-    
+
     if(linker_supports_build_id)
       set_target_properties(${lib_name} PROPERTIES LINK_FLAGS "-Wl,--build-id=0x${BUILDID}")
       set(debug_lib "${lib_name}.debug")
       add_custom_command(TARGET ${lib_name}
         POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy "$<TARGET_FILE:${lib_name}>" "${CMAKE_BINARY_DIR}/${debug_lib}"
-        
+
         # Prefer to strip via CMake install/strip target
         COMMAND ${CMAKE_STRIP} -g $<TARGET_FILE:${lib_name}>
         )
@@ -46,5 +46,5 @@ function(drishti_split_debug_symbols lib_name)
         )
     endif()
   endif()
-  
+
 endfunction(drishti_split_debug_symbols)
