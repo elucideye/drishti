cd216978e932ca0086cd8e6a1563ea4107a9f7a0
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-08-28 18:06:19 +0300

    Drishti package (some fixes) (#556)
    * Remove trailing whitespaces
    
    * Use SHA1 for 'build-id'
    
    * Install CMake configs to 'lib/cmake/<project>'
    
    See:
    * http://cgold.readthedocs.io/en/latest/tutorials/install/layout.html
    
    * Ignore 'drishti_set_hunter_version' call in Hunter

--- cmake/Modules/drishti_split_debug_symbols.cmake
+++ cmake/Modules/drishti_split_debug_symbols.cmake
@@ -19,19 +19,24 @@ function(drishti_split_debug_symbols lib_name)
   else()
 
     # see: https://sourceware.org/gdb/onlinedocs/gdb/Separate-Debug-Files.html
-    set(BUILDID "abcdef1234")
-    string(SUBSTRING "${BUILDID}" 0 2 BUILDIDPREFIX)
-    string(SUBSTRING "${BUILDID}" 2 8 BUILDIDSUFFIX)
+    string(SHA1 build_id "${PROJECT_NAME} ${lib_name}")
+
+    string(LENGTH "${build_id}" build_id_len)
+    set(prefix_len 2)
+    math(EXPR suffix_len "${build_id_len} - ${prefix_len}")
+
+    string(SUBSTRING "${build_id}" 0 ${prefix_len} BUILDIDPREFIX)
+    string(SUBSTRING "${build_id}" ${prefix_len} ${suffix_len} BUILDIDSUFFIX)
 
     include(drishti_check_cxx_linker_flag)
-    drishti_check_cxx_linker_flag("-Wl,--build-id=0x${BUILDID}" linker_supports_build_id)
+    drishti_check_cxx_linker_flag("-Wl,--build-id=0x${build_id}" linker_supports_build_id)
 
     message("linker_supports_build_id: ${linker_supports_build_id}")
-    
+
     if(linker_supports_build_id)
 
-      set_target_properties(${lib_name} PROPERTIES LINK_FLAGS "-Wl,--build-id=0x${BUILDID}")
-      
+      set_target_properties(${lib_name} PROPERTIES LINK_FLAGS "-Wl,--build-id=0x${build_id}")
+
       set(debug_lib "${lib_name}.debug")
       add_custom_command(TARGET ${lib_name}
         POST_BUILD
@@ -41,7 +46,7 @@ function(drishti_split_debug_symbols lib_name)
         # Prefer to strip via CMake install/strip target (additional flags possible here):
         COMMAND ${CMAKE_STRIP} -g "$<TARGET_FILE:${lib_name}>"
         )
-      
+
       # Install the unstripped library itself via build-id:
       install(FILES
         "${CMAKE_BINARY_DIR}/${debug_lib}"
