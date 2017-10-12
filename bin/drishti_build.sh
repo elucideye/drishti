dd2b6bef2921b5599cbfa314f8910f824b1d4d46
David Hirvonen <dhirvonen@elucideye.com>
2017-10-11 23:58:13 -0400

    refactor .travis.yml into hunter_env.sh  (setup) and drishti_build.sh for polly build command (#575)

---
+++ bin/drishti_build.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+# Simple build drishti SDK build script to support both CI builds
+# and matching host development.
+
+# Sample input parameteters:
+# TOOLCHAIN=android-ndk-r10e-api-19-armeabi-v7a-neon-hid-sections-lto
+# CONFIG=MinSizeRel
+# INSTALL=--strip
+
+if [ ${#} != 3 ]; then
+    echo 2>&1 "usage: drishti_build.sh TOOLCHAIN CONFIG INSTALL"
+    exit 1
+fi
+
+TOOLCHAIN="${1}"
+CONFIG="${2}"
+INSTALL="${3}"
+
+if [ -z "${CI}" ]; then
+    # For non-CI builds with arbitrary toolchains we don't require cache usage:
+    HUNTER_USE_CACHE_SERVERS=YES
+    HUNTER_DISABLE_BUILDS=NO
+else
+    # For CI builds with strict timeouts we must use a server side cache:
+    HUNTER_USE_CACHE_SERVERS=ONLY
+    HUNTER_DISABLE_BUILDS=YES
+fi
+
+# Note: '--ios-{multiarch,combined}' do nothing for non-iOS builds
+ARGS=(
+    --toolchain "${TOOLCHAIN}"
+    --config "${CONFIG}"
+    --verbose
+    --ios-multiarch --ios-combined
+    --fwd
+    DRISHTI_BUILD_REGRESSION_SIMD=NO
+    DRISHTI_BUILD_REGRESSION_FIXED_POINT=NO
+    DRISHTI_BUILD_TESTS=YES
+    DRISHTI_BUILD_EXAMPLES=YES
+    DRISHTI_COPY_3RDPARTY_LICENSES=ON
+    GAUZE_ANDROID_USE_EMULATOR=YES
+    HUNTER_USE_CACHE_SERVERS="${HUNTER_USE_CACHE_SERVERS}"
+    HUNTER_DISABLE_BUILDS="${HUNTER_DISABLE_BUILDS}"
+    HUNTER_CONFIGURATION_TYPES="${CONFIG}"
+    HUNTER_SUPPRESS_LIST_OF_FILES=ON
+    --archive drishti
+    --jobs 2
+    --test      
+    "${INSTALL}"
+)
+
+polly.py ${ARGS[@]} --reconfig
---
+++ bin/hunter_env.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#
+# Source: https://github.com/ruslo/hunter/blob/master/.travis.yml
+#
+# Download the latest polly release, install and use install-ci-dependencies.py
+# to install a recent CMake that is compatible with Hunter (has CURL, etc).
+# This script supports CI builds but can also be used to configure a functional
+# Hunter build environment on a host machine for standard development.
+
+# Show everything for CI debugging:
+set -x
+
+if [ -z "${CI}" ]; then
+
+    # Usage: travis_retry <flaky_command>
+    # https://github.com/t-richards/dotfiles/blob/master/.bash-functions
+    function travis_retry {
+        local result=0
+        local count=1
+        while [ $count -le 3 ]; do
+            [ $result -ne 0 ] && {
+                echo -e "\n\033[33;1mThe command \"$@\" failed. Retrying, $count of 3.\033[0m\n" >&2
+            }
+            "$@"
+            result=$?
+            [ $result -eq 0 ] && break
+            count=$(($count + 1))
+            sleep 1
+        done
+        
+        [ $count -eq 3 ] && {
+            echo "\n\033[33;1mThe command \"$@\" failed 3 times.\033[0m\n" >&2
+        }
+        return $result
+    }
+fi
+
+# Add '--quiet' to avoid leaking the token to logs
+git submodule update --init --recursive --quiet
+
+# Info about OS
+uname -a
+
+# Install Python 3
+if [[ "$(uname)" == "Darwin" ]] && [ ! $(which python3) ]; then travis_retry brew install python3; fi
+
+# Install Python package 'requests'
+# 'easy_install3' is not installed by 'brew install python3' on OS X 10.9 Maverick
+if [[ "$(uname)" == "Darwin" ]]; then pip3 install requests; fi
+if [[ "$(uname)" == "Linux" ]]; then travis_retry pip3 install --user requests; fi
+
+# Install latest Polly toolchains and scripts
+wget https://github.com/ruslo/polly/archive/master.zip
+unzip -o master.zip
+POLLY_ROOT="${PWD}/polly-master"
+export PATH="${POLLY_ROOT}/bin:${PATH}"
+
+# Install dependencies (CMake, Android NDK)
+install-ci-dependencies.py
+
+# Tune locations
+export PATH="${PWD}/_ci/cmake/bin:${PATH}"
+
+# Installed if toolchain is Android (otherwise directory doesn't exist)
+export ANDROID_NDK_r10e="${PWD}/_ci/android-ndk-r10e"
