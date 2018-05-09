23acd3d230048bc743bc310bd81589184ee492a4
David Hirvonen <dhirvonen@elucideye.com>
2018-05-09 10:55:21 -0400

    whitespace

--- bin/travis_clang_links.sh
+++ bin/travis_clang_links.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 link_dir=latest-clang-symlinks
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then 
+if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     mkdir -p ${link_dir}
     for exe in clang clang++ ; do
         ln -s /usr/bin/${exe}-5.0 ${link_dir}/${exe}
