85b1061830f290b82cb8f7f0d26ff8a0579af5ec
David Hirvonen <headupinclouds@gmail.com>
2018-05-09 12:44:53 -0700

    fix script logic/syntax for osx

--- bin/drishti_build.sh
+++ bin/drishti_build.sh
@@ -33,7 +33,7 @@ else
 fi
 
 # skip emulator CI tests for linux builds
-if [[ -v TRAVIS ]] && [[ ! -v is_linux ]]; then
+if [[ -v TRAVIS ]] && [[ ! -z ${is_linux+x} ]]; then
     GAUZE_ANDROID_USE_EMULATOR=YES # remote test w/ emulator
 else
     GAUZE_ANDROID_USE_EMULATOR=NO # support local host testing on a real device
