cb3a69a9eda6adf93e6358921c8dde9e3f7df6da
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-05-10 17:04:12 +0300

    Travis + Linux: Do not run Android tests

--- bin/drishti_build.sh
+++ bin/drishti_build.sh
@@ -18,9 +18,9 @@ CONFIG="${2}"
 INSTALL="${3}"
 
 # On CI/Travis + Linux the cpu emulation is having problems (OK on mac hosts)
-# C++ exception with description "EGLContextImpl::EGLContextImpl() : eglCreatePbufferSurface(): 
+# C++ exception with description "EGLContextImpl::EGLContextImpl() : eglCreatePbufferSurface():
 # Assertion '(eglGetError() == EGL_SUCCESS)' failed in file 'lib/aglet/EGLContext.cpp' line 59"
-# It seems the cpu based gpu emulator associated with GAUZE_ANDROID_EMULATOR_GPU=off either isn't 
+# It seems the cpu based gpu emulator associated with GAUZE_ANDROID_EMULATOR_GPU=off either isn't
 # working or isn't compatible with this eglCreatePbufferSurface() stuff, however, the build is fine
 # and the tests run on a real Android via USB connection in local host testing
 # See: https://developer.android.com/studio/run/emulator-acceleration#command-gpu
@@ -34,11 +34,14 @@ fi
 if [[ ${TRAVIS} == "true" ]]; then
   if [[ `uname` == "Linux" ]]; then
     GAUZE_ANDROID_USE_EMULATOR=NO # Not working on Linux
+    TEST=""
   else
     GAUZE_ANDROID_USE_EMULATOR=YES # remote test w/ emulator
+    TEST="--test"
   fi
 else
     GAUZE_ANDROID_USE_EMULATOR=NO # support local host testing on a real device
+    TEST="--test"
 fi
 
 # Note: '--ios-{multiarch,combined}' do nothing for non-iOS builds
@@ -49,7 +52,7 @@ ARGS=(
     --ios-multiarch --ios-combined
     --archive drishti
     --jobs 2
-    --test      
+    ${TEST}
     "${INSTALL}"
     --fwd
     DRISHTI_USE_DRISHTI_UPLOAD=YES
