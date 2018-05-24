2de6c84276e8fb3264d78f1fc3058982accf7475
David Hirvonen <dhirvonen@elucideye.com>
2018-05-23 20:53:37 -0400

    fix rpath format
    * set CMAKE_INSTALL_RPATH w/ list syntax of string syntax
    * add Context::updateGL() to allow use to call `glewInit()` from within windows DLL (requirement) for multi-context scenarios and bump minor sdk version
    * add `add_library(drishti::drishti ALIAS drishti)` in install section for `add_subdirectory()` compatibility
    * update driahti-face-test sample config file defaults
    
    ```
      set(CMAKE_INSTALL_RPATH "${DRISHTI_ORIGIN}/../lib" "${DRISHTI_ORIGIN}")
    ```

--- cmake/Modules/drishti_set_rpath.cmake
+++ cmake/Modules/drishti_set_rpath.cmake
@@ -1,9 +1,10 @@
 #https://cmake.org/Wiki/CMake_RPATH_handling
 
 macro(drishti_set_rpath)
-  set(DRISHTI_ORIGIN "$ORIGIN")
   if (APPLE)
     set(DRISHTI_ORIGIN "@loader_path")
+  else()
+    set(DRISHTI_ORIGIN "$ORIGIN")
   endif()
-  set(CMAKE_INSTALL_RPATH "${DRISHTI_ORIGIN}/../lib:${DRISHTI_ORIGIN}/")
+  set(CMAKE_INSTALL_RPATH "${DRISHTI_ORIGIN}/../lib" "${DRISHTI_ORIGIN}")
 endmacro()
