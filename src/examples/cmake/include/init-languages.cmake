62867a254520fb11092ba0f949fdb2e6bfb9fea4
David Hirvonen <dhirvonen@elucideye.com>
2018-08-26 16:50:49 -0400

    reuse cmake language block via init.cmake
    This block was duplicated in the examples tree and the top level project.  It will be useful moving forward to have a post-project install.
    
    ```
    # Languages {
    if(IOS)
      # This must be in the top/highest level directory (really)
      set(CMAKE_Swift_LANGUAGE_VERSION 4.1)
      set(CMAKE_XCODE_ATTRIBUTE_SWIFT_VERSION "4.1")
      enable_language(Swift)
    endif()
    # }
    ```
    
    * add `init-languages.cmake` and include `{src/examples}/cmake/include/init-languages.cmake` after `project()`
    * move some additional cmake setup inside the `if(NOT TARGET drishti::drishti)` check in examples folder
    * don’t build examples if `IOS AND DRISHTI_BUILD_SHARED_SDK` since we can’t run frameworks from the build tree directly — perhaps with some tinkering it can be copied properly into the application bundle from cmake

---
+++ src/examples/cmake/include/init-languages.cmake
@@ -0,0 +1,8 @@
+# Languages {
+if(IOS)
+  # This must be in the top/highest level directory (really)
+  set(CMAKE_Swift_LANGUAGE_VERSION 4.1)
+  set(CMAKE_XCODE_ATTRIBUTE_SWIFT_VERSION "4.1")
+  enable_language(Swift)
+endif()
+# }
